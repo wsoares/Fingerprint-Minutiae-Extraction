abeae750082e51fd8f0590a785eabf4d1e31724a
Wesley Soares <wesley.gbs@gmail.com>
2015-10-28 22:23:40 -0300

    Implementation of image normalization according to Fingerprint Image Enhancement and Minutiae Extraction

--- normalizeImage.m
+++ normalizeImage.m
@@ -1,7 +1,30 @@
-function [ output_args ] = normalizeImage( input_args )
+function [ normalizedImage ] = normalizeImage( originalImage )
 %NORMALIZEIMAGE Summary of this function goes here
-%   Detailed explanation goes here
+%   Normalisation is used to standardise the intensity values in an image
+%   by adjusting the range of grey-level values so that it lies within a
+%   desired range of values. The method applied here is the same presented
+%   by Thai in his report. Look at the section 2.2.2 and 2.3.3.
 
+%% Desired mean and variance values
+desiredMean = 0;
+desiredVar = 1;
 
+%% Estimation of mean and variance of originalImage
+[row col] = size(originalImage);
+vectorImage = im2single(reshape(originalImage', 1, row*col)); %   Converting the originalImage in a vector with single values
+varImage = var(vectorImage,1);
+meanImage = mean(vectorImage);
+
+%% Estimation of normalization pixel-wise
+for r=1 : row
+    for c=1 : col
+        if originalImage(r,c) > meanImage
+            normalizedImage(r,c) = desiredMean + sqrt((desiredVar*(im2single(originalImage(r,c)) - desiredMean)^2)/varImage);
+        else
+            normalizedImage(r,c) = desiredMean - sqrt((desiredVar*(im2single(originalImage(r,c)) - desiredMean)^2)/varImage);
+        end
+    end
+end
+normalizedImage = mat2gray(normalizedImage);
 end
 
