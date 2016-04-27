b705583a985200088680535b441ca0accbcc4d0c
Wesley Soares <wesley.gbs@gmail.com>
2016-04-27 00:12:27 -0300

    Verifies the number of arguments, if the dimensions of image are divisible by the window input. Applied open e close filters to perform a better segmentation

--- segmentImage.m
+++ segmentImage.m
@@ -2,29 +2,56 @@
 %   This function separates the foreground regions in the fringerprint
 %   image from the background. The foreground is the area of interest.
 
-function [sgmtd_img] = segmentImage( img )
-
-window = 20;
-sgmtd_img = img;
-img = double(img);
-bw = ones(size(img));
+function [mask] = segmentImage(img, window)
+%% Verifying the number of inputs
+if nargin ~= 2
+    error('Number of arguments MUST be equal 2');
+end
+%% Converting the image values in a double
+if ~isa(img, 'double')
+    img = double(img);
+end
+%% Adjusting the size of the image according the window
 [row,col] = size(img);
-threshold = var(img(:),1);  %calculating the global variance
-
+x = mod([row,col],window);
+if x(1) ~= 0    %including news rows if it isn't multiple of window
+    qtdRows = row - x(1);
+    newRow = img(row,1:col);
+    for i=1:qtdRows
+        img = vertcat(img,newRow);
+    end
+end
+if x(2) ~= 0    %including news cols if it isn't multiple of window
+    [row,col] = size(img);
+    qtdCols = col - x(2);
+    newCol = img(1:row,col);
+    for i=1:qtdCols
+        img = horzcat(img,newCol);
+    end
+end
+%% Calculating the variance of the image which it will be used as a threshold
+threshold = var(img(:),1);
 if threshold >= 0.1
     threshold = threshold/100;
 end
-
+%% Segmenting the image and creating the mask
+[row,col] = size(img);
+mask = ones(row,col);
 for i=1:row/window
     r = [1+(i-1)*window,i*window];
     for j=1:col/window        
         c = [1+(j-1)*window,j*window];
         block = img(r(1):r(2),c(1):c(2));
         if var(block(:),1) < threshold
-            sgmtd_img(r(1):r(2),c(1):c(2)) = 0;
-            bw(r(1):r(2),c(1):c(2)) = 0;
+            mask(r(1):r(2),c(1):c(2)) = 0;
         end
     end
 end
+%% Applying close and opening operations.
+% These operations will make holes dissapear and the mask will be more
+% uniform
+se = strel('disk', 5, 0);
+mask = imclose(mask,se);
+mask = imopen(mask,se);
 end
 
